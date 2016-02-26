d6f1c1add6da5d2eee85da043dcdbd8f4fbb1c75
Michael Henry Tirkey <michael.tirkey2012@yahoo.com>
2016-02-26 15:40:33 -0500

    Adding initial files

---
+++ ContentDownloader/ContentDownloader/app/src/main/java/com/mike/givemewingzz/contentdownloader/content/ContentDownloader.java
@@ -0,0 +1,7 @@
+package com.mike.givemewingzz.contentdownloader.content;
+
+/**
+ * Created by GiveMeWingzz on 2/26/2016.
+ */
+public class ContentDownloader {
+}
---
+++ ContentDownloader/ContentDownloader/app/src/main/java/com/mike/givemewingzz/contentdownloader/utils/ContentDownloaderUtils.java
@@ -0,0 +1,7 @@
+package com.mike.givemewingzz.contentdownloader.utils;
+
+/**
+ * Created by GiveMeWingzz on 2/26/2016.
+ */
+public class ContentDownloaderUtils {
+}
